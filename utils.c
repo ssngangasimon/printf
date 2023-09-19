991c3cbf1d99c64092e302df2511fdf658a8230a
Simon Nganga <ngangasimoncs@gmail.com>
2023-09-19 12:11:21 -0700

    utils

---
+++ utils.c
@@ -0,0 +1,87 @@
+#include "main.h"
+
+/**
+ * is_printable - evaluate if char is printable
+ * @c: char
+ *
+ * Return: 1 if c is printable, 0 otherwise
+ */
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * append_hexa_code - append ascci in hexadecimal notation
+ * @buffer: array of chars
+ * @i: index to start appending.
+ * @ascii_code: ASSCI CODE
+ * Return: Always 3
+ */
+int append_hexa_code(char ascii_code, char buffer[], int i)
+{
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code is always 2 digits long */
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
+	return (3);
+}
+
+/**
+ * is_digit - Verifies if a char is a digit
+ * @c: Char to be evaluated
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+
+/**
+ * convert_size_number - cast number to the specified size
+ * @num: number
+ * @size: number of type casted
+ *
+ * Return: casted value of number
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
+}
+
+/**
+ * convert_size_unsgnd - cast a number to the specified size
+ * @num: number
+ * @size: number of type casted
+ *
+ * Return: casted value of number
+ */
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
+}
+
